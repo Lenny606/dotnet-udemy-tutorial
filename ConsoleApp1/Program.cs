aba6dc43c3dc7f8363c813f0a27b4986240937fc
Tomas Kravcik <kravcik@procorp.cz>
2025-11-29 21:07:28 +0100

    Add weather data calculations and countdown functions.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -27,12 +27,43 @@ class Program
             temp[i] = rnd.Next(-10, 30);
             actualConditions[i] = conditions[rnd.Next(conditions.Length)];
         }
+
+        CountDown(days);
         
         Console.WriteLine($"Average temperature: {GetAvgTemp(temp)}");
+        Console.WriteLine($"Minimum temperature: {GetMinTemp(temp)}");
+        Console.WriteLine($"Maximum temperature: {GetMaxTemp(temp)}");
+        Console.WriteLine($"Most common cond: {GetMostCommonCondition(actualConditions)}");
 
 
     }
+    static string GetMostCommonCondition(string[] conditions)
+    {
+        
+        return conditions.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
+    }
     
+    static int GetMinTemp(int[] temp)
+    {
+        return temp.Min();
+    }
+    static int GetMaxTemp(int[] temp)
+    {
+        return temp.Max();
+    }
+
+    static void CountDown(int num)
+    {
+        if (num <= 0)
+        {
+            Console.WriteLine("Starting calculating weather data...");
+            return;
+        }
+
+        Console.WriteLine("Ccounting down: {0}", num);
+        
+        CountDown(num - 1);
+    }
     
     static double GetAvgTemp(int[] temp)
     {
