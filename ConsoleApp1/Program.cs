10929ef8758df48f429b3c46908ece10a2a40916
Tomas Kravcik <kravcik@procorp.cz>
2025-11-29 20:48:24 +0100

    Initialize solution with .gitignore, projects, and sample code.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp1;
+
+//WEATHER PROJECT
+class Program
+{
+    static int days;
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number of days: ");
+       
+        var ok = int.TryParse(Console.ReadLine(), out days);
+        
+        if (!ok)
+        {
+            Console.WriteLine("Invalid input");
+            return;
+        }
+
+        int[] temp = new int[days];
+        string[] conditions = { "Rainy", "Sunny", "Snowy" };
+        string[] actualConditions = new string[days];
+        
+        Random rnd = new Random();
+
+        for (int i = 0; i < days; i++)
+        {
+            temp[i] = rnd.Next(-10, 30);
+            actualConditions[i] = conditions[rnd.Next(conditions.Length)];
+        }
+        
+        Console.WriteLine($"Average temperature: {GetAvgTemp(temp)}");
+
+
+    }
+    
+    
+    static double GetAvgTemp(int[] temp)
+    {
+        return temp.Average();
+    }
+}
---
+++ DotnetUdemy/Test.cs
@@ -0,0 +1,22 @@
+﻿namespace DotnetUdemy;
+
+class Test
+{
+    static int _result;//field is instance
+
+    static void Main(string[] args)
+    {
+        static void Add(int a, int b)
+        {
+            _result = a + b;
+            // return _result;
+        }
+
+        static void PrintResult()
+        {
+            Add(5, 4);
+            Console.WriteLine(_result);
+        }
+        PrintResult();
+    }
+}
